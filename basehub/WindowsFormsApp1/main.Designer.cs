931c453a51c34ea4a20a1be62b9313125eecd012
Kru3mel <malte.jung@online.de>
2021-05-14 00:38:36 +0200

    added map feature
    - added map loading feature for a desired location using Google Cloud Platform - Maps Static API

--- basehub/WindowsFormsApp1/main.Designer.cs
+++ basehub/WindowsFormsApp1/main.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace WindowsFormsApp1
+namespace basehub
 {
     partial class main
     {
@@ -29,20 +29,60 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.pictureBox_Map = new System.Windows.Forms.PictureBox();
+            this.Button_Search = new System.Windows.Forms.Button();
+            this.TextBox_Location = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Map)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // pictureBox_Map
+            // 
+            this.pictureBox_Map.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Map.Image")));
+            this.pictureBox_Map.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox_Map.Name = "pictureBox_Map";
+            this.pictureBox_Map.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox_Map.TabIndex = 0;
+            this.pictureBox_Map.TabStop = false;
+            // 
+            // Button_Search
+            // 
+            this.Button_Search.Location = new System.Drawing.Point(357, 658);
+            this.Button_Search.Name = "Button_Search";
+            this.Button_Search.Size = new System.Drawing.Size(75, 23);
+            this.Button_Search.TabIndex = 1;
+            this.Button_Search.Text = "Suchen";
+            this.Button_Search.UseVisualStyleBackColor = true;
+            this.Button_Search.Click += new System.EventHandler(this.Button_Search_Click);
+            // 
+            // TextBox_Location
+            // 
+            this.TextBox_Location.Location = new System.Drawing.Point(12, 658);
+            this.TextBox_Location.Name = "TextBox_Location";
+            this.TextBox_Location.Size = new System.Drawing.Size(339, 23);
+            this.TextBox_Location.TabIndex = 2;
+            // 
+            // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1503, 927);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.TextBox_Location);
+            this.Controls.Add(this.Button_Search);
+            this.Controls.Add(this.pictureBox_Map);
+            this.Name = "main";
             this.Text = "Hub";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Map)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Map;
+        private System.Windows.Forms.Button Button_Search;
+        private System.Windows.Forms.TextBox TextBox_Location;
     }
 }
 
--- basehub/WindowsFormsApp1/main.cs
+++ basehub/WindowsFormsApp1/main.cs
@@ -8,14 +8,58 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp1
+using System.Net.Http;
+using System.IO;
+
+namespace basehub
 {
     public partial class main : Form
     {
+        HttpClient httpClient = new HttpClient();
+
         public main()
-        {
+        {            
             InitializeComponent();
         }
 
+        private void Button_Search_Click(object sender, EventArgs e)
+        {
+            string location = TextBox_Location.Text;
+            string size = "640x640";
+            string scale = "1";
+            string zoom = "14";
+            string apiKey = "AIzaSyAqM1CnlEfs669Rig4zOzec48UusxS_hhI";
+            string mapType = "roadmap";
+
+            UriBuilder uriBuilder = new UriBuilder();
+            uriBuilder.Scheme = "http";
+            uriBuilder.Host = "maps.googleapis.com";
+            uriBuilder.Path = "/maps/api/staticmap";
+            uriBuilder.Query = $"center={location}&size={size}&scale={scale}&zoom={zoom}&maptype={mapType}&key={apiKey}";
+
+            var image = Image.FromStream(HttpGetBytes(uriBuilder.Uri));
+            pictureBox_Map.Image = image;
+        }
+
+        #region methods
+        /*
+        private string HttpGet(Uri uri)
+        {
+
+
+            return data;
+        }*/     
+
+        private Stream HttpGetBytes(Uri uri)
+        {
+            byte[] byte_data = httpClient.GetByteArrayAsync(uri).Result;
+            Stream data = new MemoryStream(byte_data);
+            return data;
+        }     
+
+        #endregion
+
     }
 }
+
+
