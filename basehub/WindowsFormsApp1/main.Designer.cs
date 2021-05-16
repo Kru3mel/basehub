bce72d5e75e9ceaa72792f984cac07a82edffb6e
Kru3mel <malte.jung@online.de>
2021-05-16 19:43:08 +0200

    -added geocoding api
    -added geocoding api to get coordinates of map center

--- basehub/WindowsFormsApp1/main.Designer.cs
+++ basehub/WindowsFormsApp1/main.Designer.cs
@@ -29,40 +29,44 @@ namespace basehub
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox_Map = new System.Windows.Forms.PictureBox();
+            this.pictureBox_map = new System.Windows.Forms.PictureBox();
             this.Button_Search = new System.Windows.Forms.Button();
-            this.TextBox_Location = new System.Windows.Forms.TextBox();
+            this.textBox_location = new System.Windows.Forms.TextBox();
             this.groupBox_MapData = new System.Windows.Forms.GroupBox();
+            this.button_load = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox_MapType = new System.Windows.Forms.ComboBox();
+            this.comboBox_mapType = new System.Windows.Forms.ComboBox();
             this.groupBox_MapControls = new System.Windows.Forms.GroupBox();
-            this.numericUpDown_MapSize = new System.Windows.Forms.NumericUpDown();
-            this.trackBar_MapZoom = new System.Windows.Forms.TrackBar();
+            this.numericUpDown_mapSize = new System.Windows.Forms.NumericUpDown();
+            this.trackBar_mapZoom = new System.Windows.Forms.TrackBar();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox_Telemetry = new System.Windows.Forms.GroupBox();
+            this.comboBox_SelectDorne = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Map)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_map)).BeginInit();
             this.groupBox_MapData.SuspendLayout();
             this.groupBox_MapControls.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_MapSize)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar_MapZoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_mapSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_mapZoom)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox_Telemetry.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox_Map
+            // pictureBox_map
             // 
-            this.pictureBox_Map.Location = new System.Drawing.Point(6, 15);
-            this.pictureBox_Map.Name = "pictureBox_Map";
-            this.pictureBox_Map.Size = new System.Drawing.Size(1128, 909);
-            this.pictureBox_Map.TabIndex = 0;
-            this.pictureBox_Map.TabStop = false;
+            this.pictureBox_map.Location = new System.Drawing.Point(6, 15);
+            this.pictureBox_map.Name = "pictureBox_map";
+            this.pictureBox_map.Size = new System.Drawing.Size(1128, 909);
+            this.pictureBox_map.TabIndex = 0;
+            this.pictureBox_map.TabStop = false;
             // 
             // Button_Search
             // 
+            this.Button_Search.Enabled = false;
             this.Button_Search.Location = new System.Drawing.Point(514, 37);
             this.Button_Search.Name = "Button_Search";
             this.Button_Search.Size = new System.Drawing.Size(81, 31);
@@ -71,22 +75,24 @@ namespace basehub
             this.Button_Search.UseVisualStyleBackColor = true;
             this.Button_Search.Click += new System.EventHandler(this.Button_Search_Click);
             // 
-            // TextBox_Location
+            // textBox_location
             // 
-            this.TextBox_Location.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.TextBox_Location.Location = new System.Drawing.Point(6, 37);
-            this.TextBox_Location.Multiline = true;
-            this.TextBox_Location.Name = "TextBox_Location";
-            this.TextBox_Location.Size = new System.Drawing.Size(375, 31);
-            this.TextBox_Location.TabIndex = 2;
+            this.textBox_location.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_location.Location = new System.Drawing.Point(6, 37);
+            this.textBox_location.Multiline = true;
+            this.textBox_location.Name = "textBox_location";
+            this.textBox_location.Size = new System.Drawing.Size(375, 31);
+            this.textBox_location.TabIndex = 2;
             // 
             // groupBox_MapData
             // 
+            this.groupBox_MapData.Controls.Add(this.button_load);
+            this.groupBox_MapData.Controls.Add(this.button_save);
             this.groupBox_MapData.Controls.Add(this.label2);
             this.groupBox_MapData.Controls.Add(this.label1);
-            this.groupBox_MapData.Controls.Add(this.comboBox_MapType);
+            this.groupBox_MapData.Controls.Add(this.comboBox_mapType);
             this.groupBox_MapData.Controls.Add(this.Button_Search);
-            this.groupBox_MapData.Controls.Add(this.TextBox_Location);
+            this.groupBox_MapData.Controls.Add(this.textBox_location);
             this.groupBox_MapData.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.groupBox_MapData.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.groupBox_MapData.ImeMode = System.Windows.Forms.ImeMode.Disable;
@@ -96,6 +102,24 @@ namespace basehub
             this.groupBox_MapData.TabIndex = 4;
             this.groupBox_MapData.TabStop = false;
             this.groupBox_MapData.Text = "Map Data Controls";
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(863, 37);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(81, 31);
+            this.button_load.TabIndex = 6;
+            this.button_load.Text = "Load";
+            this.button_load.UseVisualStyleBackColor = true;
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(776, 37);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(81, 31);
+            this.button_save.TabIndex = 5;
+            this.button_save.Text = "Save";
+            this.button_save.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -115,26 +139,27 @@ namespace basehub
             this.label1.TabIndex = 3;
             this.label1.Text = "Loaction";
             // 
-            // comboBox_MapType
+            // comboBox_mapType
             // 
-            this.comboBox_MapType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox_MapType.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.comboBox_MapType.FormattingEnabled = true;
-            this.comboBox_MapType.ItemHeight = 23;
-            this.comboBox_MapType.Items.AddRange(new object[] {
+            this.comboBox_mapType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_mapType.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox_mapType.FormattingEnabled = true;
+            this.comboBox_mapType.ItemHeight = 23;
+            this.comboBox_mapType.Items.AddRange(new object[] {
             "roadmap",
             "satelllite",
             "terrain",
             "hybrid"});
-            this.comboBox_MapType.Location = new System.Drawing.Point(387, 37);
-            this.comboBox_MapType.Name = "comboBox_MapType";
-            this.comboBox_MapType.Size = new System.Drawing.Size(121, 31);
-            this.comboBox_MapType.TabIndex = 0;
+            this.comboBox_mapType.Location = new System.Drawing.Point(387, 37);
+            this.comboBox_mapType.Name = "comboBox_mapType";
+            this.comboBox_mapType.Size = new System.Drawing.Size(121, 31);
+            this.comboBox_mapType.TabIndex = 0;
+            this.comboBox_mapType.SelectedIndexChanged += new System.EventHandler(this.comboBox_MapType_SelectedIndexChanged);
             // 
             // groupBox_MapControls
             // 
-            this.groupBox_MapControls.Controls.Add(this.numericUpDown_MapSize);
-            this.groupBox_MapControls.Controls.Add(this.trackBar_MapZoom);
+            this.groupBox_MapControls.Controls.Add(this.numericUpDown_mapSize);
+            this.groupBox_MapControls.Controls.Add(this.trackBar_mapZoom);
             this.groupBox_MapControls.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.groupBox_MapControls.Location = new System.Drawing.Point(1158, 12);
             this.groupBox_MapControls.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
@@ -144,42 +169,42 @@ namespace basehub
             this.groupBox_MapControls.TabStop = false;
             this.groupBox_MapControls.Text = "Map Controls";
             // 
-            // numericUpDown_MapSize
+            // numericUpDown_mapSize
             // 
-            this.numericUpDown_MapSize.Location = new System.Drawing.Point(25, 506);
-            this.numericUpDown_MapSize.Maximum = new decimal(new int[] {
+            this.numericUpDown_mapSize.Location = new System.Drawing.Point(25, 506);
+            this.numericUpDown_mapSize.Maximum = new decimal(new int[] {
             20,
             0,
             0,
             0});
-            this.numericUpDown_MapSize.Name = "numericUpDown_MapSize";
-            this.numericUpDown_MapSize.Size = new System.Drawing.Size(45, 23);
-            this.numericUpDown_MapSize.TabIndex = 2;
-            this.numericUpDown_MapSize.Value = new decimal(new int[] {
+            this.numericUpDown_mapSize.Name = "numericUpDown_mapSize";
+            this.numericUpDown_mapSize.Size = new System.Drawing.Size(45, 23);
+            this.numericUpDown_mapSize.TabIndex = 2;
+            this.numericUpDown_mapSize.Value = new decimal(new int[] {
             15,
             0,
             0,
             0});
-            this.numericUpDown_MapSize.ValueChanged += new System.EventHandler(this.numericUpDown_MapSize_ValueChanged);
+            this.numericUpDown_mapSize.ValueChanged += new System.EventHandler(this.numericUpDown_MapSize_ValueChanged);
             // 
-            // trackBar_MapZoom
+            // trackBar_mapZoom
             // 
-            this.trackBar_MapZoom.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.trackBar_MapZoom.LargeChange = 3;
-            this.trackBar_MapZoom.Location = new System.Drawing.Point(25, 22);
-            this.trackBar_MapZoom.Maximum = 20;
-            this.trackBar_MapZoom.Name = "trackBar_MapZoom";
-            this.trackBar_MapZoom.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar_MapZoom.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.trackBar_MapZoom.Size = new System.Drawing.Size(45, 478);
-            this.trackBar_MapZoom.TabIndex = 1;
-            this.trackBar_MapZoom.Value = 15;
-            this.trackBar_MapZoom.ValueChanged += new System.EventHandler(this.trackBar_MapZoom_ValueChanged);
+            this.trackBar_mapZoom.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.trackBar_mapZoom.LargeChange = 3;
+            this.trackBar_mapZoom.Location = new System.Drawing.Point(25, 22);
+            this.trackBar_mapZoom.Maximum = 20;
+            this.trackBar_mapZoom.Name = "trackBar_mapZoom";
+            this.trackBar_mapZoom.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar_mapZoom.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar_mapZoom.Size = new System.Drawing.Size(45, 478);
+            this.trackBar_mapZoom.TabIndex = 1;
+            this.trackBar_mapZoom.Value = 15;
+            this.trackBar_mapZoom.ValueChanged += new System.EventHandler(this.trackBar_MapZoom_ValueChanged);
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.White;
-            this.groupBox1.Controls.Add(this.pictureBox_Map);
+            this.groupBox1.Controls.Add(this.pictureBox_map);
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
@@ -192,6 +217,7 @@ namespace basehub
             // 
             // groupBox_Telemetry
             // 
+            this.groupBox_Telemetry.Controls.Add(this.comboBox_SelectDorne);
             this.groupBox_Telemetry.Controls.Add(this.dataGridView1);
             this.groupBox_Telemetry.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.groupBox_Telemetry.Location = new System.Drawing.Point(1258, 12);
@@ -201,13 +227,22 @@ namespace basehub
             this.groupBox_Telemetry.TabStop = false;
             this.groupBox_Telemetry.Text = "UAS Telemetry";
             // 
+            // comboBox_SelectDorne
+            // 
+            this.comboBox_SelectDorne.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox_SelectDorne.FormattingEnabled = true;
+            this.comboBox_SelectDorne.Location = new System.Drawing.Point(6, 22);
+            this.comboBox_SelectDorne.Name = "comboBox_SelectDorne";
+            this.comboBox_SelectDorne.Size = new System.Drawing.Size(198, 31);
+            this.comboBox_SelectDorne.TabIndex = 1;
+            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 67);
+            this.dataGridView1.Location = new System.Drawing.Point(6, 59);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(622, 462);
+            this.dataGridView1.Size = new System.Drawing.Size(622, 470);
             this.dataGridView1.TabIndex = 0;
             // 
             // groupBox2
@@ -235,13 +270,13 @@ namespace basehub
             this.MaximumSize = new System.Drawing.Size(1920, 1080);
             this.Name = "main";
             this.Text = "BaseHub";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Map)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_map)).EndInit();
             this.groupBox_MapData.ResumeLayout(false);
             this.groupBox_MapData.PerformLayout();
             this.groupBox_MapControls.ResumeLayout(false);
             this.groupBox_MapControls.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_MapSize)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar_MapZoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_mapSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_mapZoom)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox_Telemetry.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -251,21 +286,25 @@ namespace basehub
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox_Map;
+        private System.Windows.Forms.PictureBox pictureBox_map;
         private System.Windows.Forms.Button Button_Search;
-        private System.Windows.Forms.TextBox TextBox_Location;
+        private System.Windows.Forms.TextBox textBox_location;
         private System.Windows.Forms.Button Button_Save;
         private System.Windows.Forms.GroupBox groupBox_MapData;
         private System.Windows.Forms.GroupBox groupBox_MapControls;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox_Telemetry;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TrackBar trackBar_MapZoom;
+        private System.Windows.Forms.TrackBar trackBar_mapZoom;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.NumericUpDown numericUpDown_MapSize;
-        private System.Windows.Forms.ComboBox comboBox_MapType;
+        private System.Windows.Forms.NumericUpDown numericUpDown_mapSize;
+        private System.Windows.Forms.ComboBox comboBox_mapType;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.ComboBox comboBox_SelectDorne;
+        private System.Windows.Forms.Button button_save;
     }
 }
 
--- basehub/WindowsFormsApp1/main.cs
+++ basehub/WindowsFormsApp1/main.cs
@@ -10,12 +10,14 @@ using System.Windows.Forms;
 
 using System.Net.Http;
 using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace basehub
 {
     public partial class main : Form
     {
         HttpClient httpClient = new HttpClient();
+
 
         public main()
         {            
@@ -24,31 +26,71 @@ namespace basehub
 
         private void Button_Search_Click(object sender, EventArgs e)
         {
-            string location = TextBox_Location.Text;
-            //string size = "640x640";
-            string scale = GetMapSize(pictureBox_Map.Width, pictureBox_Map.Height, 1, out string size).ToString();
-            string zoom = trackBar_MapZoom.Value.ToString();
+            string location = textBox_location.Text;
+            string size;
+            string scale = GetMapSize(pictureBox_map.Width, pictureBox_map.Height, 1, out size).ToString();
+            string zoom = trackBar_mapZoom.Value.ToString();
             string apiKey = "AIzaSyC4BIZBnmh0nkMR2u7wtrk0eQtwublOG9c";
             string mapType = "hybrid";
 
-            UriBuilder uriBuilder = new UriBuilder();
-            uriBuilder.Scheme = "http";
-            uriBuilder.Host = "maps.googleapis.com";
-            uriBuilder.Path = "/maps/api/staticmap";
-            uriBuilder.Query = $"center={location}&size={size}&scale={scale}&zoom={zoom}&maptype={mapType}&key={apiKey}";
+            UriBuilder imageUri = new UriBuilder();
+            imageUri.Scheme = "http";
+            imageUri.Host = "maps.googleapis.com";
+            imageUri.Path = "/maps/api/staticmap";
+            imageUri.Query = $"center={location}&size={size}&scale={scale}&zoom={zoom}&maptype={mapType}&key={apiKey}";
 
-            var image = Image.FromStream(HttpGetStream(uriBuilder.Uri));
-            pictureBox_Map.Image = image;
+            UriBuilder locationUri = new UriBuilder();
+            locationUri.Scheme = "https";
+            locationUri.Host = "maps.googleapis.com";
+            locationUri.Path = "/maps/api/geocode/json";
+            locationUri.Query = $"address={location}&key={apiKey}";
+
+            /*
+            var image = Image.FromStream(HttpGetStream(imageUri.Uri));
+            pictureBox_map.Image = image;*/
+
+            if (HttpGetString(locationUri.Uri,out string data))
+            {
+                JObject locationData = JObject.Parse(data);
+                string latitude = (string)locationData["results"][0]["geometry"]["location"]["lat"];
+                string longitude = (string)locationData["results"][0]["geometry"]["location"]["lng"];
+            };
+            
+        }
+
+        private void trackBar_MapZoom_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown_mapSize.Value = trackBar_mapZoom.Value;
+        }
+
+        private void numericUpDown_MapSize_ValueChanged(object sender, EventArgs e)
+        {
+            trackBar_mapZoom.Value = (int)numericUpDown_mapSize.Value;
+        }
+        private void comboBox_MapType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Button_Search.Enabled = true;
         }
 
         #region methods
-        /*
-        private string HttpGet(Uri uri)
+
+        private bool HttpGetString(Uri uri, out string data)
         {
-
-
-            return data;
-        }*/     
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(uri).Result;
+                response.EnsureSuccessStatusCode();
+                data = response.Content.ReadAsStringAsync().Result;
+                return true;
+            }
+            catch(Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.ToString());
+                data = "";
+                return false;
+            }
+            
+        }
 
         private Stream HttpGetStream(Uri uri)
         {
@@ -71,15 +113,7 @@ namespace basehub
 
         #endregion
 
-        private void trackBar_MapZoom_ValueChanged(object sender, EventArgs e)
-        {
-            numericUpDown_MapSize.Value = trackBar_MapZoom.Value;
-        }
-
-        private void numericUpDown_MapSize_ValueChanged(object sender, EventArgs e)
-        {
-            trackBar_MapZoom.Value = (int)numericUpDown_MapSize.Value;
-        }
+        
     }
 }
 
