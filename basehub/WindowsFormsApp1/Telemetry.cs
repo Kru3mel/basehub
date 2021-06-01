2565a89436f152aa0644131be619976401b9a292
Kru3mel <malte.jung@online.de>
2021-06-02 01:10:36 +0200

    Create Telemetry.cs

---
+++ basehub/WindowsFormsApp1/Telemetry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basehub
+{
+    public class Telemetry
+    {
+        public Telemetry()
+        {
+
+        }
+
+        public Telemetry(string name,double latitude,double longitude, int height, int velocity, string heading, int battery)
+        {
+            this.Name = name;
+            this.Longitude = longitude;
+            this.Longitude = latitude;
+            this.Height = height;
+            this.Velocity = velocity;
+            this.Heading = heading;
+            this.Battery = battery;
+        }
+
+        public string Name { set; get; }
+
+        public double Latitude { set; get; }
+
+        public double Longitude { set; get; }
+
+        public int Height { set; get; }
+
+        public int Velocity { set; get;}
+
+        public string Heading { set; get; }
+
+        public int Battery { set; get; }
+    }
+}
