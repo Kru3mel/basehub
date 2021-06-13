b1cf96b3a5940a1fc812c573777e07e4f95554ee
Kru3mel <malte.jung@online.de>
2021-06-13 23:51:52 +0200

    Create SqLiteDataBase.cs
    Added Class do deal with SQLite Connection

---
+++ basehub/WindowsFormsApp1/classes/SqLiteDataBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SQLite;
+
+namespace basehub.classes
+{
+    class SqLiteDataBase
+    {
+        public SqLiteDataBase()
+        {
+
+        }
+
+        public string Path { set; get; }
+
+        public string connectionString { set; get; }
+
+        public void CreateDatabase(string path)
+        {
+            Path = path;
+
+            if (!File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+            }
+
+            connectionString = $"Data Source={path};Version=3;";
+
+            return;
+        }
+
+        public void CreateTable()
+        {
+            SQLiteConnection connection = new SQLiteConnection(connectionString);
+            connection.Open();
+
+            connection.Close();
+        }
+
+    }
+}
